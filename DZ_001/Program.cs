bc797968c61b84b0a85fe39e507d4e3894f0451a
MaxX <maxx-f@mail.ru>
2022-09-02 08:34:18 +0300

    Add DZ_001

---
+++ DZ_001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int start, int end)
+{
+    if (start > end)
+    return;
+
+    Console.Write($"{start} ");
+    PrintNumbers(start + 1, end);
+}
+
+PrintNumbers(m, n);
